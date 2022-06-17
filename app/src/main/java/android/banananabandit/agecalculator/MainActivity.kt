cadc464cb07629b5185e79d8977c4a197a6bd55b
Pavel Cistjakov <pavelcistjakov@gmail.com>
2022-06-17 16:14:35 +0100

    Adding UI elements and implementing a Spinner

--- app/src/main/java/android/banananabandit/agecalculator/MainActivity.kt
+++ app/src/main/java/android/banananabandit/agecalculator/MainActivity.kt
@@ -2,10 +2,39 @@ package android.banananabandit.agecalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val spinner = findViewById<Spinner>(R.id.units_selector)
+        spinner.onItemSelectedListener = this
+
+        val spinnerAdapter : ArrayAdapter<*> = ArrayAdapter<Any?>(
+            this, android.R.layout.simple_spinner_item, units
+        )
+
+        spinnerAdapter.setDropDownViewResource(
+            android.R.layout.simple_spinner_dropdown_item
+        )
+        spinner.adapter = spinnerAdapter
+
+    }
+    var units = arrayOf<String?>("Months", "Weeks", "Days", "Hours", "Minutes")
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        Toast.makeText(applicationContext,
+            units[position],
+            Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+        TODO("Not yet implemented")
     }
 }
